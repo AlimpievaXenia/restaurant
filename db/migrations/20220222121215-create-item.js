637e1ffd41e3f67c29e37cb63a289669d73cc314
Iskandar <iskan.vokh@outlook.com>
2022-02-22 16:07:28 +0100

    DB

---
+++ db/migrations/20220222121215-create-item.js
@@ -0,0 +1,51 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      ingredients: {
+        type: Sequelize.STRING,
+      },
+      price: {
+        type: Sequelize.STRING,
+      },
+      weight: {
+        type: Sequelize.STRING,
+      },
+      proteins: {
+        type: Sequelize.STRING,
+      },
+      fats: {
+        type: Sequelize.STRING,
+      },
+      carbs: {
+        type: Sequelize.STRING,
+      },
+      colories: {
+        type: Sequelize.STRING,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Items');
+  },
+};
---
+++ db/migrations/20220222121959-create-order-item.js
@@ -0,0 +1,31 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('OrderItems', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      itemId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      orderId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('OrderItems');
+  },
+};
