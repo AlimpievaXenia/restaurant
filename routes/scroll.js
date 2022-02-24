d877392eab0123c402a2d0be3348ad64eb1009a1
Insaeva <becketova.ek@yandex.ru>
2022-02-24 20:28:11 +0300

    HerokuDeploy

--- routes/scroll.js
+++ routes/scroll.js
@@ -4,7 +4,7 @@ const { Category } = require('../db/models');
 
 router.get('/scroll', async (req, res) => {
   const categories = await Category.findAll({ raw: true });
-  // console.log('categories >>>', categories);
+  console.log('categories >>>', categories);
   res.render('scroll', { categories });
 });
 
